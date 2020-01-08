75b9946c488cdfe2f3a0802828d774ff72a2079e
Houssam Jelliti <houssamje@xtech.guru>
2020-01-08 15:39:58 +0100

    Process and skip comments and HTML5 DTD
    This commit contains feature addition to process comments and HTML5 DTD and skip them.
    It also contains some automatic changes from double to single quotes and perhaps others
    due to some automatic linting with VSCode.
    
    The skipping decision here is because I am not sure what are the benefits of
    returning a tree with comments and DTD.
    
    You can review the tests to see the addition of comments and doctype test cases.
    Currently, thio could support only HTML5 (browser-based) documents DTDs.
    This will close #3 and close #2 but probably in the future I will open new issues
    with new HTML DTDs processing capabilities with other HTML versions.

---
+++ src/compiler/lexer/comments.js
@@ -0,0 +1,14 @@
+export default ({ input, cursor, tokens }) => {
+  while (
+    input.slice(cursor, cursor + 3) !== '-->' &&
+    input[cursor] !== undefined
+  ) {
+    // skip comments
+    cursor++;
+  }
+  if (input.slice(cursor, cursor + 3) === '-->') {
+    cursor += 3;
+  }
+
+  return cursor;
+};
