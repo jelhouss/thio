bb6ba8a89c325220a02b8f08e8c07f1f52e2fdaa
Houssam Jelliti <jelitihoussam@gmail.com>
2020-12-21 22:27:19 +0100

    Nuke whole setup

--- src/compiler/constants/regex.js
+++
@@ -1,8 +0,0 @@
-/**
- *
- * All regex pattern needed goes here.
- */
-
-export const ATTRIBUTE_VALUE_REGEX = /^[^<]+$/
-export const DATA_ATTRIBUTE_NAME_REGEX = /^[a-z]+(-{1}[a-z]+)*$/
-export const DATA_ATTRIBUTE_VALUE_REGEX = /^([a-z]+|[0-9]+)$/
--- src/compiler/parser/text.js
+++
@@ -1,3 +0,0 @@
-export default (token) => ({
-  Text: token.text
-})
